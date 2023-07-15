f465eaf273629985282387395b714639288a890d
Mayur Hanwate <frostblizard111@gmail.com>
2023-07-15 15:37:35 +0530

    restart and maxScore logic added

--- src/Board.java
+++ src/Board.java
@@ -15,6 +15,9 @@ public class Board extends JPanel implements ActionListener {
     int MAX_DOTS = 1600;
     int DOTS;
 
+    //Max Score
+    int maxScore = 0;
+
     //Initilizing X and Y Arrays
     int x[] = new int[MAX_DOTS];
     int y[] = new int[MAX_DOTS];
@@ -47,7 +50,6 @@ public class Board extends JPanel implements ActionListener {
     JLabel text;
 
     //First Game counter
-    boolean firstGame = true;
 
     Board(){
         TAdapter tAdapter = new TAdapter();
@@ -57,17 +59,16 @@ public class Board extends JPanel implements ActionListener {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.BLACK);
 
-        if(firstGame){
-            firstGame = false;
+
             setButton();
-        }
 
     }
 
-    Board(boolean ifTrue){
+    Board(int maxScore){
         TAdapter tAdapter = new TAdapter();
         addKeyListener(tAdapter);
         setFocusable(true);
+        this.maxScore = maxScore;
 
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.BLACK);
@@ -177,15 +178,21 @@ public class Board extends JPanel implements ActionListener {
     public void gameOver(Graphics g){
         String msg = "Game Over";
         int score = (DOTS-3)*100;
+        if(score > maxScore){
+            maxScore = score;
+        }
         String scoreMsg = "Score: "+score;
+        String maxScoreMsg = "Max Score: "+maxScore;
         Font small = new Font("Helvetica", Font.BOLD,14);
         FontMetrics fontMetrics = getFontMetrics(small);
+
 
         g.setColor(Color.WHITE);
         g.setFont(small);
 
         g.drawString(msg, (WIDTH - fontMetrics.stringWidth(msg))/2, 50);
         g.drawString(scoreMsg, (WIDTH - fontMetrics.stringWidth(scoreMsg))/2, 150);
+        g.drawString(maxScoreMsg, (WIDTH - fontMetrics.stringWidth(maxScoreMsg))/2, 100);
 
 
         //Restart Button
@@ -196,12 +203,12 @@ public class Board extends JPanel implements ActionListener {
 
         Font buttonFont = new Font("Arial", Font.BOLD, 14);
         restartButton.setFont(buttonFont);
-        restartButton.setForeground(Color.WHITE);
+        restartButton.setForeground(Color.GREEN);
 
         restartButton.setPreferredSize(new Dimension(100,40));
 
         setLayout(null);
-        restartButton.setBounds(150,150,100,40);
+        restartButton.setBounds(150,250,100,40);
         add(restartButton);
 
         restartButton.addActionListener(new ActionListener() {
@@ -209,7 +216,7 @@ public class Board extends JPanel implements ActionListener {
             public void actionPerformed(ActionEvent e) {
                 Container parent = getParent();
                 parent.remove(Board.this);
-                Board newBoard = new Board(true);
+                Board newBoard = new Board(maxScore);
                 parent.add(newBoard);
                 parent.revalidate();
                 newBoard.requestFocusInWindow();
